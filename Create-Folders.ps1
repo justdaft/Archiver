195de1a6ec7e8a2e10ed688ce08efa61ce7e66be
billy westbury <billy.westbury@gmail.com>
2012-10-24 16:35:31 +0100

    create folders

---
+++ Create-Folders.ps1
@@ -0,0 +1,14 @@
+﻿function Create-folders ($file=".\FoldersLIst.txt", $root=".\")
+{
+    $collection = Get-Content $file
+    Write-Debug "using `$collection $collection"
+    foreach ($item in $collection)
+    {
+       write-debug "creating `$root$item $root$item"
+       new-item -ItemType directory $root$item 
+    }
+    
+       
+}
+
+Create-folders -file .\FoldersLIst.txt -root c:\test\
